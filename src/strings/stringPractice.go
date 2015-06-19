3df6a3b887b0bcbeb96fdecb55f9486bbde49611
Jai Sun <jsun@nyu.edu>
2015-06-18 20:39:32 -0400

    woah

--- src/strings/stringPractice.go
+++ src/strings/stringPractice.go
@@ -24,27 +24,34 @@ func main() {
 	//fmt.Printf(f, c, c)
 	fmt.Printf("hmmm %i, %s, %s", a[0], b[1], c[2]) //these shows the int value of the characters
 	fmt.Println(c[0], c[6], c[7])
+	fmt.Println("\n===============================\n")
 	
-	fmt.Println(strings.Contains(a, "good"))
-	
+	//Contains
+	fmt.Println(strings.Contains(a, "d m")) //true
 	if strings.Contains(a, "good") == true {
 		fmt.Printf("'%v' contains the word 'good'\n", a)
 	}
+	fmt.Println("\n===============================\n")
 	
 	//ContainsAny
-	fmt.Println(strings.ContainsAny("team", "i"))
-	fmt.Println(strings.ContainsAny("failure", "u & i"))
-	fmt.Println(strings.ContainsAny("foo", ""))
+	fmt.Println("fmt.Println(strings.ContainsAny('team', 'i & n'))")
+	fmt.Println(strings.ContainsAny("team", "i & n & a")) //if at least one of these are in it then its true.
+	fmt.Println(strings.ContainsAny("failure", "z"))
+	fmt.Println(strings.ContainsAny("foo", "foooasdf")) //this is also true. okay Go
 	fmt.Println(strings.ContainsAny("", ""))
+	fmt.Println("\n===============================\n")
+	
 	//ContainsRune
 	//var tester rune = 103 //should return true
 	var tester rune = 10 //should return false
 	another := strings.ContainsRune(a, tester)
 	fmt.Println(another)
+	fmt.Println("\n===============================\n")
 	
 	//Count
 	fmt.Println(strings.Count(c, "")) //returns the length of string
 	fmt.Println(strings.Count(c, "a")) //returns the number of 'a'
+	fmt.Println("\n===============================\n")
 	
 	//EqualFold
 	fmt.Println(strings.EqualFold("Go", "go")) //true
@@ -52,6 +59,7 @@ func main() {
 	fmt.Println(strings.EqualFold("우리나라", "우리나라")) //true
 	fmt.Println(strings.EqualFold("우리나라", "ㅇㅜ린ㅏ라")) //false
 	fmt.Println(strings.EqualFold("싸랑해사랑해", "사랑해싸랑해")) //false
+	fmt.Println("\n===============================\n")
 	
 	//Fields
 	fmt.Println("Original string = '   foo bar  baz   '")
